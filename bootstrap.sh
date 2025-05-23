3213485a43685780bf695a477f37bac1fab2f9e1
Brandon Tillman <brandon@brandontillman.com>
2025-05-23 12:58:38 -0700

    Added boostrap file and arma-reforger server

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # ENVIRONMENT VARIABLES used in script (.env file)
-# SS_STEAMCMD_USER
-# SS_STEAM_SERVER_PATH
-# SS_VALIDATE_DOCKER_INSTALL
-# SS_USE_EXISTING_DOCKER_INSTALL
+# STEAMCMD_USER
+# STEAM_SERVER_PATH
+# VALIDATE_DOCKER_INSTALL
+# USE_EXISTING_DOCKER_INSTALL
 
 # Default values
-SS_STEAMCMD_USER="steamcmd"
-SS_STEAM_SERVER_PATH="/steam/servers"
-SS_VALIDATE_DOCKER_INSTALL=true
-SS_USE_EXISTING_DOCKER_INSTALL=false
+STEAMCMD_USER="steamcmd"
+STEAM_SERVER_PATH="/steam/servers"
+VALIDATE_DOCKER_INSTALL=true
+USE_EXISTING_DOCKER_INSTALL=false
 
 # Check if .env file exists
 if [ -f ".env" ]; then
@@ -26,7 +26,7 @@ then
     install_docker
 else
     echo "Docker is already installed..."
-    if [ "$SS_USE_EXISTING_DOCKER_INSTALL" = !true ]; then
+    if [ "$USE_EXISTING_DOCKER_INSTALL" = !true ]; then
         echo "Please remove any previous Docker installations before running this script."
         exit 1
     fi
@@ -34,9 +34,9 @@ fi
 
 # Create steamcmd user and directory
 echo "Creating steamcmd user and directory..."
-sudo useradd -m $SS_STEAMCMD_USER
+sudo useradd -m $STEAMCMD_USER
 sudo mkdir -p /steam/servers
-sudo chown -R $SS_STEAMCMD_USER:$SS_STEAMCMD_USER /steam
+sudo chown -R $STEAMCMD_USER:$STEAMCMD_USER /steam
 
 # Create Portainer container
 echo "Creating Portainer container..."
@@ -72,7 +72,7 @@ install_docker() {
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # Validate Docker installation
-    if [ "$SS_VALIDATE_DOCKER_INSTALL" = true ]; then
+    if [ "$VALIDATE_DOCKER_INSTALL" = true ]; then
         if ! sudo docker run hello-world &> /dev/null
         then
             echo "Docker installation failed, please check the logs."
